6254b73db06a52696f22c1ae510bdb082f1f1143
David Darx <daviddarx@gmail.com>
2019-12-08 18:35:32 +0100

    feat(general) hello-world vue.js

---
+++ src/app.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div>
+    Je suis une app vue {{test2}} {{test1}}
+  </div>
+</template>
+
+<script>
+
+  import Vue from "vue";
+
+  export default Vue.extend({
+    components: {
+    },
+    data() {
+      return {
+        test2: "test 2"
+      }
+    },
+    computed: {
+      test1: function() {
+        return 'test 1';
+      },
+    },
+    mounted () {
+      this.init();
+    },
+    methods: {
+      init: function () {
+        console.log("test");
+      }
+    }
+  });
+</script>
