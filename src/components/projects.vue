b1380a026a1986fccdebe744df20b615276dff6e
David Darx <daviddarx@gmail.com>
2019-12-24 14:03:23 +0100

    feat(transitions) makes a hack to animate in projects without scrolling problems

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,15 +1,25 @@
 
 <template>
-  <div class="projects">
-    <project-list
-      class="projects-list"
-      v-for="project in projects.main"
-      v-bind:key="project.title"
-      v-bind:datas="project"
+  <div
+    class="projects"
+    ref="projects"
+    v-bind:style="{height: this.height}"
+  >
+    <div
+      class="projects__container"
+      ref="projectsContainer"
+      v-bind:class="{ 'is-translated': this.isTranslated, 'is-masked': this.isMasked }"
     >
-    </project-list>
-    <div class="credits">
-      Copyright © 2019 - Frontend von David Darx
+      <project-list
+        class="projects-list"
+        v-for="project in projects.main"
+        v-bind:key="project.title"
+        v-bind:datas="project"
+      >
+      </project-list>
+      <div class="credits">
+        Copyright © 2019 - Frontend von David Darx
+      </div>
     </div>
   </div>
 </template>
@@ -25,12 +35,29 @@
     },
     data() {
       return {
-        projects: projects
+        projects: projects,
+        height: 'auto',
+        isTranslated: false,
+        isMasked: true
       }
     },
     mounted () {
+      //hack to be able to translateY the projects for animation in
+      //without having trouble with first scroll position of app
+      this.height = getComputedStyle(this.$refs.projects).height;
+
+      setTimeout(this.animateProjectsIn, 150);
     },
     methods: {
+      animateProjectsIn: function () {
+        this.isTranslated = true;
+        this.isMasked = false;
+
+        setTimeout(() => {
+          this.isTranslated = false;
+          this.height = 'auto';
+        }, 100);
+      }
     }
   });
 </script>
