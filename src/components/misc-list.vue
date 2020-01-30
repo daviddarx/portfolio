e42dc75e242ba51e533053350928ea3bd6cae5ba
David Darx <daviddarx@gmail.com>
2020-01-30 20:50:58 +0100

    feat(misc) implements global pixi to create all filter canvas

--- src/components/misc-list.vue
+++ src/components/misc-list.vue
@@ -2,9 +2,7 @@
 <template>
   <div
     class="misc-list miscs-grid__item"
-    v-bind:class="{ 'is-displayed' : this.isLoaded }"
-    @mouseover="imageMouseOver"
-    @mouseleave="imageMouseOut"
+    v-bind:class="{ 'is-displayed' : this.isReady }"
   >
     <router-link
       v-bind:to="route"
@@ -19,6 +17,11 @@
           @load="imageLoaded"
           ref="image"
         >
+        <canvas
+          class="misc-list__img-canvas"
+          ref="canvas"
+        >
+        </canvas>
       </div>
       <h2
         class="misc-list__info"
@@ -32,11 +35,6 @@
 </template>
 
 <script>
-  import {AdjustmentFilter} from '@pixi/filter-adjustment';
-  import * as PIXI from 'pixi.js';
-  import gsap, {Power2} from 'gsap';
-
-
   export default {
     name: 'misc-list',
     props: {
@@ -44,28 +42,11 @@
       imgURL: String,
       date: String,
       title: String,
+      id: Number
     },
     data: function () {
       return {
-        isLoaded: false,
-        imgWidth: 0,
-        imgHeight: 0,
-        imgRatioHtoW: 0,
-        imgScale: 0,
-        imgDimensions: undefined,
-        canvas:undefined,
-        pixiApp:undefined,
-        pixiTexture: undefined,
-        pixiTextureBase: undefined,
-        pixiSprite: undefined,
-        pixiEase: Power2,
-        pixiScaleOnHover: 0.9,
-        pixiScaleAnimationDuration: 0.3,
-        pixiFilterAnimationDuration: 0.5,
-        pixiFilter: undefined,
-        pixiFilterSaturation: 0,
-        pixiFilterContrast: 2,
-        pixiFilterBrightness: 2,
+        isReady: false
       }
     },
     mounted () {
@@ -75,115 +56,21 @@
         this.$refs.image.setAttribute('src', this.imgURL);
       },
       imageLoaded: function () {
-        this.isLoaded = true;
-        this.imgWidth = this.$refs.image.naturalWidth;
-        this.imgHeight = this.$refs.image.naturalHeight;
-        this.imgRatioHtoW = this.imgHeight / this.imgWidth;
-        this.initPixi();
-        window.addEventListener('resize', this.resize);
-        this.setupImage();
         this.$emit('loaded');
       },
-      getImageComputedDimensions: function () {
+      getImageDimensions: function () {
         return {
-          width: this.$refs.imageContainer.clientWidth,
-          height: this.$refs.imageContainer.clientWidth * this.imgRatioHtoW
-        }
+          width: this.$refs.image.clientWidth,
+          height: this.$refs.image.clientHeight
+        };
       },
-      resize: function () {
-        this.pixiApp.view.style.display = 'none';
-        requestAnimationFrame(() => {
-          this.imgDimensions = this.getImageComputedDimensions();
-          this.pixiApp.renderer.resize(this.imgDimensions.width, this.imgDimensions.height);
-          this.pixiApp.view.style.display = 'block';
-          this.resizeImage();
-        });
+      cloneCanvas: function (pixiCanvas) {
+        this.$refs.canvas.width = pixiCanvas.width;
+        this.$refs.canvas.height = pixiCanvas.height;
+        this.$refs.canvas.getContext('2d').drawImage(pixiCanvas, 0, 0);
       },
-      initPixi: function () {
-        this.imgDimensions = this.getImageComputedDimensions();
-        this.pixiApp = new PIXI.Application({
-          width: this.imgDimensions.width,
-          height: this.imgDimensions.height,
-          backgroundColor: '0xffffff',
-          antialias: true,
-          resolution: window.devicePixelRatio
-        });
-        this.pixiApp.renderer.autoResize = true;
-        this.$refs.imageContainer.appendChild(this.pixiApp.view);
-        this.pixiApp.view.classList.add('misc-list__img-canvas');
-        this.pixiFilter = new AdjustmentFilter();
-        this.pixiFilter.saturation = this.pixiFilterSaturation;
-        this.pixiFilter.contrast = this.pixiFilterContrast;
-        this.pixiFilter.brightness = this.pixiFilterBrightness;
-        this.stopPixi();
-      },
-      startPixi: function() {
-        this.pixiApp.start();
-      },
-      stopPixi: function() {
-        this.pixiApp.stop();
-      },
-      setupImage: function () {
-        this.pixiTextureBase = new PIXI.BaseTexture(this.$refs.image);
-        //remettre loader
-        console.log(PIXI.utils.TextureCache);
-        this.pixiTexture = new PIXI.Texture(this.pixiTextureBase),
-        this.pixiSprite = new PIXI.Sprite(this.pixiTexture);
-
-        this.pixiSprite.filters = [this.pixiFilter];
-        this.pixiApp.stage.addChild(this.pixiSprite);
-        this.resizeImage();
-      },
-      renderImage: function () {
-        this.pixiApp.renderer.render( this.pixiApp.stage);
-      },
-      resizeImage: function () {
-        this.pixiSprite.width = this.imgDimensions.width;
-        this.pixiSprite.height = this.imgDimensions.height;
-        this.pixiSprite.x = this.imgDimensions.width * 0.5;
-        this.pixiSprite.y = this.imgDimensions.height * 0.5;
-        this.pixiSprite.anchor.x = this.pixiSprite.anchor.y = 0.5;
-        this.imgScale = this.pixiSprite.scale.x;
-        this.renderImage();
-      },
-      imageMouseOver: function () {
-        this.killTweens();
-        this.startPixi();
-        gsap.to(this.pixiSprite.scale, {
-          duration: this.pixiScaleAnimationDuration,
-          ease:this.pixiEase.easeOut,
-          x: this.imgScale * this.pixiScaleOnHover,
-          y: this.imgScale * this.pixiScaleOnHover
-        });
-        gsap.to(this.pixiFilter, {
-          duration: this.pixiFilterAnimationDuration,
-          saturation: 1,
-          contrast:1,
-          brightness: 1,
-          onComplete: this.stopPixi
-        });
-      },
-      imageMouseOut: function() {
-        this.killTweens();
-        this.startPixi();
-        gsap.to(this.pixiSprite.scale, {
-          duration: this.pixiScaleAnimationDuration,
-          ease:this.pixiEase.easeOut,
-          x: this.imgScale,
-          y: this.imgScale
-        });
-        gsap.to(this.pixiFilter, {
-          duration: this.pixiFilterAnimationDuration,
-          saturation: this.pixiFilterSaturation,
-          contrast: this.pixiFilterContrast,
-          brightness: this.pixiFilterBrightness,
-          onComplete: this.stopPixi
-        });
-      },
-      killTweens: function () {
-        gsap.killTweensOf(this.pixiSprite.scale);
-        gsap.killTweensOf(this.pixiFilter);
-        gsap.killTweensOf(this.stopPixi);
+      setReady: function () {
+        this.isReady = true;
       }
     }
   }
--- src/components/misc.vue
+++ src/components/misc.vue
@@ -8,16 +8,16 @@
       <p class="content-page__lead animate-in">
         {{misc.lead}}
       </p>
-
       <div class="content-page__3-4 miscs__container miscs-grid animate-in animate-in__s1">
         <misc-list
-          v-for="miscItem in misc.collection"
+          v-for="(miscItem, index) in misc.collection"
           v-bind:key="miscItem.title"
           v-bind:route="misc.routesPath + miscItem.uid"
           v-bind:imgURL="misc.mediasPath+miscItem.thumb"
           v-bind:date="miscItem.date"
           v-bind:title="miscItem.title"
-          v-on:loaded="loadNextMisc"
+          v-bind:id="index"
+          v-on:loaded="loadCompleteListener"
           ref="miscItem"
         >
         </misc-list>
@@ -34,6 +34,9 @@
   import * as misc from '../../content/misc.json';
   import dash from '../mixins/dash';
 
+  import {AdjustmentFilter} from '@pixi/filter-adjustment';
+  import * as PIXI from 'pixi.js';
+
   export default Vue.extend({
     components: {
       'misc-list': MiscList,
@@ -45,6 +48,14 @@
         misc: misc,
         isDisplayed : false,
         miscItemLoadID: 0,
+        pixi: {
+          sprites: [],
+          settings: {
+            saturation: 0,
+            contrast: 2,
+            brightness: 2
+          }
+        },
       }
     },
     created () {
@@ -53,7 +64,18 @@
       });
     },
     mounted () {
+      this.initPixi();
+      window.addEventListener('resize', this.resize);
       setTimeout(this.displayMisc, 100);
+    },
+    beforeRouteLeave (to, from, next) {
+      this.pixi.app.destroy(true, {
+        children: true,
+        texture: false,
+        baseTexture: false
+      });
+      window.removeEventListener('resize', this.resize);
+      next();
     },
     methods: {
       displayMisc: function() {
@@ -61,11 +83,74 @@
 
         this.$refs.miscItem[0].launchLoading();
       },
-      loadNextMisc: function () {
+      initPixi: function() {
+        this.pixi.app = new PIXI.Application({
+          width: 1,
+          height: 1,
+          antialias: true,
+          resolution: window.devicePixelRatio
+        });
+        this.pixi.app.renderer.autoResize = true;
+        this.pixi.app.stop();
+
+        this.pixi.filter = new AdjustmentFilter();
+        this.pixi.filter.saturation = this.pixi.settings.saturation;
+        this.pixi.filter.contrast = this.pixi.settings.contrast;
+        this.pixi.filter.brightness = this.pixi.settings.brightness;
+      },
+      setPixiImage: function (miscItem) {
+        const sprite = new PIXI.Sprite(PIXI.Loader.shared.resources[miscItem.imgURL].texture);
+              sprite.filters = [this.pixi.filter];
+
+        this.pixi.sprites[miscItem.id] = sprite;
+
+        this.drawPixiImage(miscItem);
+
+        miscItem.cloneCanvas(this.pixi.app.view);
+        miscItem.setReady();
+
+        this.loadNext();
+      },
+      drawPixiImage: function (miscItem) {
+        const imgSize = miscItem.getImageDimensions();
+
+        this.pixi.sprite = this.pixi.sprites[miscItem.id];
+        this.pixi.app.stage.addChild(this.pixi.sprite);
+
+        this.pixi.app.renderer.resize(imgSize.width, imgSize.height);
+        this.pixi.sprite.width = imgSize.width;
+        this.pixi.sprite.height = imgSize.height;
+
+        this.renderPixi();
+        this.pixi.app.stage.removeChild(this.pixi.sprite);
+      },
+      renderPixi: function() {
+        this.pixi.app.renderer.render( this.pixi.app.stage);
+      },
+      loadCompleteListener: function () {
+        const miscItem = this.$refs.miscItem[this.miscItemLoadID]
+
+        if (PIXI.Loader.shared.resources[miscItem.imgURL] == undefined) {
+          PIXI.Loader.shared.add(miscItem.imgURL).load(() => {
+            this.setPixiImage(miscItem);
+          });
+        } else {
+          this.setPixiImage(miscItem);
+        }
+      },
+      loadNext: function () {
         if (this.miscItemLoadID < this.$refs.miscItem.length-1) {
           this.miscItemLoadID++;
           this.$refs.miscItem[this.miscItemLoadID].launchLoading();
         }
+      },
+      resize: function () {
+        this.$refs.miscItem.forEach(miscItem => {
+          if (this.pixi.sprites[miscItem.id]){
+            this.drawPixiImage(miscItem);
+            miscItem.cloneCanvas(this.pixi.app.view);
+          }
+        });
       }
     }
   });
