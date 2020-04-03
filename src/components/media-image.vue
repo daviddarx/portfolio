6282389bf431381b8519ba9b092fa137fc12cd47
David Darx <daviddarx@gmail.com>
2020-04-03 19:17:49 +0200

    feat(media-image) adds preloader

--- src/components/media-image.vue
+++ src/components/media-image.vue
@@ -2,7 +2,9 @@
 <template>
   <div
     class="media-image"
+    v-bind:class="{'is-loaded' : isLoaded}"
   >
+    <preloader class="media-image__preloader"></preloader>
     <img
       v-bind:src="this.url"
       v-bind:alt="this.title"
@@ -13,24 +15,29 @@
 </template>
 
 <script>
+  import Preloader from './preloader.vue';
+
   export default {
     name: 'media-image',
+    components: {
+      'preloader': Preloader
+    },
     props: {
       url: String,
       title: String
     },
     data: function () {
       return {
+        isLoaded: false
       }
     },
     mounted () {
     },
     methods: {
       imageLoaded: function () {
-        console.log("loaded image " + this.title);
+        this.isLoaded = true;
       },
       destroy: function () {
-        console.log("destroy image " + this.title);
       }
     }
   }
---
+++ src/components/preloader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="preloader"
+  >
+      <svg class="preloader__svg" viewBox="25 25 50 50">
+        <circle
+          class="preloader__path"
+          cx="50"
+          cy="50"
+          r="20"
+          fill="none"
+          stroke-miterlimit="10"
+        />
+      </svg>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'preloader',
+    data: function () {
+      return {
+      }
+    },
+    mounted () {
+    },
+    methods: {
+    }
+  }
+</script>
