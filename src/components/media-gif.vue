c9e0e1589e3ef8fabe04c998c781ac233ff62dda
David Darx <daviddarx@gmail.com>
2020-04-18 14:09:27 +0200

    feat(info) adds basic implementation of media-gif

---
+++ src/components/media-gif.vue
@@ -0,0 +1,51 @@
+
+<template>
+  <div
+    class="media-gif"
+    v-bind:class="{'is-loaded' : isLoaded}"
+    ref="container"
+  >
+    <preloader class="media-gif__preloader preloader--inverted"></preloader>
+    <img
+      v-bind:src="this.path+this.images[0]"
+      v-bind:alt="this.title"
+      @load="imageLoaded"
+      class="media-gif__el"
+      ref="image"
+    >
+  </div>
+</template>
+
+<script>
+  import Preloader from './preloader.vue';
+
+  export default {
+    name: 'media-gif',
+    components: {
+      'preloader': Preloader
+    },
+    props: {
+      path: String,
+      title: String,
+      images: Array
+    },
+    data: function () {
+      return {
+        isLoaded: false,
+      }
+    },
+    mounted () {
+      console.log(this.images);
+    },
+    methods: {
+      imageLoaded: function () {
+        this.isLoaded = true;
+        console.log("loaded");
+      },
+      destroy: function () {
+        //todo
+        //keep alive pour continuer le loading?
+      }
+    }
+  }
+</script>
