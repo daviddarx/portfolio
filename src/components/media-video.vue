5bdd4c646321939353fd4dd9acbdcb55297cd78a
David Darx <daviddarx@gmail.com>
2020-05-12 20:42:27 +0200

    fix(media-video) sets playing on reenter after canplaythrough

--- src/components/media-video.vue
+++ src/components/media-video.vue
@@ -37,26 +37,29 @@ import { TextureUvs } from 'pixi.js';
       return {
         isLoading: false,
         isPlaying: false,
-        bufferedPercent: 0
+        isCanplaythrough: false,
+        bufferedPercent: 0,
       }
     },
     mounted () {
       this.$refs.video.addEventListener('waiting', this.onWaiting);
       this.$refs.video.addEventListener('playing', this.onPlaying);
-      this.$refs.video.addEventListener('progress', this.progressListener);
-      this.$refs.video.addEventListener('canplay', this.canplayListener);
+      this.$refs.video.addEventListener('progress', this.progressListener); // to remove
       this.$refs.video.addEventListener('canplaythrough', this.canplaythroughListener);
-      this.$refs.video.addEventListener('suspend', this.suspendListener);
     },
     methods: {
       enter: function () {
         if (this.autoplay == true) {
           this.isPlaying = true;
-          this.isLoading = true,
           console.log("enter");
+
+          if (this.isCanplaythrough == true) {
+            this.play();
+          }
         }
       },
       leave: function () {
+        console.log("leave");
         this.isPlaying = false;
         this.pause();
       },
@@ -76,14 +79,11 @@ import { TextureUvs } from 'pixi.js';
       progressListener: function () {
         console.log('progress: ' + this.buffered());
       },
-      canplayListener: function () {
-        console.log('canplay: ' + this.buffered());
-      },
       canplaythroughListener: function () {
         if (this.isPlaying == true) {
           this.play();
-          console.log("launch play");
         }
+        this.isCanplaythrough = true;
         console.log('canplaythrough: ' + this.buffered());
       },
       suspendListener: function () {
@@ -100,10 +100,8 @@ import { TextureUvs } from 'pixi.js';
         this.pause();
         this.$refs.video.removeEventListener('waiting', this.onWaiting);
         this.$refs.video.removeEventListener('playing', this.onPlaying);
-        this.$refs.video.removeEventListener('loadedmetadata', this.metadataListener);
-        this.$refs.video.removeEventListener('canplay', this.canplayListener);
+        this.$refs.video.removeEventListener('progress', this.progressListener); // to remove
         this.$refs.video.removeEventListener('canplaythrough', this.canplaythroughListener);
-        this.$refs.video.removeEventListener('suspend', this.suspendListener);
       }
     }
   }
