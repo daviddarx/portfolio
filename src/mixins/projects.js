bcf4b98164b0c3600ea29bb08b631651687d15eb
David Darx <daviddarx@gmail.com>
2020-06-05 11:25:21 +0200

    feat(projects) adds scroll animate in mixins

--- src/mixins/projects.js
+++ src/mixins/projects.js
@@ -2,28 +2,88 @@ export default {
   data() {
     return {
       isMounted: false,
+      refsArray: undefined,
+      observer: undefined,
+      observerItems: undefined,
+      observerRootMargin: "-10%" //20vh to compensate
     }
   },
   methods: {
     mount () {
       this.isMounted = true;
+      requestAnimationFrame(() => {
+        this.refsArray = Object.values(this.$refs);
+        this.setIntersectionObserver();
+      });
       window.addEventListener('resize', this.resize);
     },
+    setIntersectionObserver () {
+      this.observerItems = document.body.querySelectorAll('.observed');
+
+      if (!!window.IntersectionObserver) {
+        this.observer = new IntersectionObserver(this.intersectionListener, {
+          rootMargin: this.observerRootMargin
+        });
+
+        this.observerItems.forEach(item => {
+          this.observer.observe(item);
+        });
+      } else {
+        this.observerItems.forEach(item => {
+          item.classList.add('is-displayed');
+        });
+      }
+    },
+    intersectionListener: function (entries, observer) {
+      entries.forEach(entry => {
+        const isVideo = entry.target.hasAttribute('isvideo');
+        let videoComponent;
+
+        if (isVideo == true) {
+          const videoID = entry.target.getAttribute('id');
+          this.refsArray.forEach((item) => {
+            if (item.$el.getAttribute('id') == videoID) {
+              videoComponent = item;
+            }
+          });
+        }
+
+        if(entry.isIntersecting){
+          entry.target.classList.add('is-displayed');
+
+          if (isVideo == false) {
+            observer.unobserve(entry.target);
+          } else {
+            videoComponent.enter();
+          }
+        } else {
+          if (isVideo == true) {
+            videoComponent.leave();
+          }
+        }
+      });
+    },
     resize () {
-      const refsArray = Object.values(this.$refs);
-      refsArray.forEach(ref => {
+      this.refsArray.forEach(ref => {
         if (ref.resize) {
           ref.resize();
         }
       });
     },
     destroy () {
-      const refsArray = Object.values(this.$refs);
-      refsArray.forEach(ref => {
+      this.refsArray.forEach(ref => {
         if (ref.destroy) {
           ref.destroy();
         }
       });
+      this.refsArray = [];
+
+      if (this.observer) {
+        this.observerItems.forEach(item => {
+          this.observer.unobserve(item);
+        });
+        this.observer.disconnect();
+      }
 
       window.removeEventListener('resize', this.resize);
     }
