623df6e731f24498426b00bb608f25e9088c8cec
David Darx <daviddarx@gmail.com>
2020-07-16 12:36:30 +0200

    feat(transition-title) makes all transitions the same, with first wave black too

--- src/components/transition-background.vue
+++ src/components/transition-background.vue
@@ -22,8 +22,6 @@
       <div
         class="transition-background__el-in"
         v-if="this.isWaveInDisplayed"
-        v-bind:style="{backgroundColor: this.color}"
-        v-bind:class="{ 'is-normal-page': this.isNormalPage}"
       >
       </div>
     </transition>
@@ -34,7 +32,6 @@
       <div
         class="transition-background__el-out"
         v-if="this.isWaveOutDisplayed"
-        v-bind:class="{ 'is-normal-page': this.isNormalPage}"
       >
       </div>
     </transition>
@@ -51,11 +48,9 @@
     },
     data() {
       return {
-        color: '#000000',
         isWaveInDisplayed: false,
         isWaveOutDisplayed: false,
         isLogoDisplayed: false,
-        isNormalPage: false,
         isTitleDisplayed: false,
         titleValue: '',
         nextFunction: undefined
@@ -67,26 +62,17 @@
     methods: {
       beforeEach: function (to, from, next) {
         this.titleValue = to.meta.transitionTitle;
-        if(to.meta.color){
-          this.color = to.meta.color;
-          this.isNormalPage = false;
-        } else {
-          this.color = undefined;
-          this.isNormalPage = true;
-          this.isLogoDisplayed = true;
-          this.isTitleDisplayed = true;
-        }
+        this.isLogoDisplayed = true;
+        this.isTitleDisplayed = true;
         this.isWaveInDisplayed = true;
         this.nextFunction = next;
       },
       waveInAfterEnter: function() {
         this.isWaveInDisplayed = false;
         this.nextFunction();
-        if (this.isNormalPage == true) {
-          this.isWaveOutDisplayed = true;
-          this.isLogoDisplayed = false;
-          this.isTitleDisplayed = false;
-        }
+        this.isWaveOutDisplayed = true;
+        this.isLogoDisplayed = false;
+        this.isTitleDisplayed = false;
       },
       waveOutAfterEnter: function() {
         this.isWaveOutDisplayed = false;
