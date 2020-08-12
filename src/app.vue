dc78aa19d324c5829dbedfaa6a080f5ac62139de
David Darx <daviddarx@gmail.com>
2020-08-12 16:19:19 +0200

    feat(nav) implements vertical nav for responsive

--- src/app.vue
+++ src/app.vue
@@ -4,8 +4,8 @@
     class="website-container"
   >
     <h1 class="visually-hidden">David Darx</h1>
-    <logo></logo>
-    <custom-navigation></custom-navigation>
+    <logo ref="logo"></logo>
+    <custom-navigation ref="nav"></custom-navigation>
     <keep-alive :include="['misc-keep', 'info-keep']">
       <router-view :key="$route.fullPath"></router-view>
     </keep-alive>
@@ -36,6 +36,8 @@
       this.$router.beforeEach(this.beforeEach);
       this.checkIfUserTouch();
       this.checkIfDarkMode(this.$router.currentRoute.meta.isDark);
+      setTimeout(this.resize, 200);
+      window.addEventListener('resize', this.resize);
     },
     methods: {
       checkIfUserTouch: function () {
@@ -53,6 +55,20 @@
         } else {
           document.body.classList.remove('is-dark');
         }
+      },
+      checkVerticalNav: function () {
+        const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+        const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+        const gutter = parseFloat(getComputedStyle(this.$refs.logo.$el).getPropertyValue('--s-gutter').split('vw')[0])/100 * vh;
+
+        this.$refs.nav.$el.classList.remove('vertical');
+
+        if (this.$refs.logo.$el.offsetWidth + this.$refs.nav.$el.offsetWidth + 2 * gutter > vh - gutter * 0.5){
+          this.$refs.nav.$el.classList.add('vertical');
+        }
+      },
+      resize: function () {
+        this.checkVerticalNav();
       },
       beforeEach: function (to, from, next) {
         if (from.name == 'projects' || from.name == 'misc') {
