8fdcd188f3a284b90db7dff77bb8745fdd5454f7
David Darx <daviddarx@gmail.com>
2020-09-05 00:08:22 +0200

    test

--- src/components/home.vue
+++ src/components/home.vue
@@ -6,25 +6,27 @@
     v-bind:class="{ 'is-displayed': this.isDisplayed }"
   >
   ../../static/projects/ar/scenes_overview.mp4<br>
-  <video width="400" controls ref="video1">
+  <video width="400" controls >
     <source src="../../static/projects/ar/scenes_overview.mp4" type="video/mp4">
     Your browser does not support HTML video.
   </video>
 
   <br><br>
   https://files.daviddarx.com/stuffs/testvideo/scenes_overview.mp4<br>
-  <video width="400" controls ref="video1">
+  <video width="400" controls >
     <source src="https://files.daviddarx.com/stuffs/testvideo/scenes_overview.mp4" type="video/mp4">
     Your browser does not support HTML video.
   </video>
 
 
   <br><br>
-  https://files.daviddarx.com/stuffs/testvideo/scenes_overview.mp4<br>
-  <video width="400" controls ref="video1">
-    <source src="https://files.daviddarx.com/stuffs/testvideo/scenes_overview.mp4" type="video/mp4">
+  https://deploy-preview-12--daviddarx-portfolio.netlify.app/projects/ar/scenes_overview.mp4<br>
+  <video width="400" controls >
+    <source src="https://deploy-preview-12--daviddarx-portfolio.netlify.app/projects/ar/scenes_overview.mp4" type="video/mp4">
     Your browser does not support HTML video.
   </video>
+
+
 
   <!-- https://deploy-preview-12--daviddarx-portfolio.netlify.app/projects/ar/scenes_overview.mp4 -->
 
@@ -50,11 +52,11 @@
     mounted () {
       setTimeout(this.displayHome, 100);
 
-      console.log("test 7");
+      console.log("test 8");
 
-      this.$refs.video1.addEventListener("canplay", function() {
-        console.log("oncanplay1");
-      });
+//       this.$refs.video1.addEventListener("canplay", function() {
+//         console.log("oncanplay1");
+//       });
 
 //       this.$refs.video2.addEventListener("canplay", function() {
 //         console.log("oncanplay2");
