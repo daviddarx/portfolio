be36eabe07cbf73798dab42386e217a20a0fe377
daviddarx <daviddarx@gmail.com>
2021-02-19 16:02:32 +0100

    feat(home) adds video

--- src/components/home.vue
+++ src/components/home.vue
@@ -15,7 +15,24 @@
       <router-link
         to="/projects"
         class="home__stage animate-in animate-in__s2"
+        ref="stage"
       >
+        <video
+          muted
+          autoplay
+          playsinline
+          loop
+          preload="auto"
+          poster = "https://files.daviddarx.com/works/static/home/home.jpg"
+          class="home__video"
+          ref="video"
+        >
+          <source
+            src = "https://files.daviddarx.com/works/static/home/home.mp4"
+            type = "video/mp4"
+          >
+        <video>
+
       </router-link>
 
     </div>
@@ -29,17 +46,55 @@
     data() {
       return {
         isDisplayed : false,
+        stageW: 0,
+        stageH: 0,
+        stageRatio: 0,
+        videoW: 0,
+        videoH: 0,
+        videoX: 0,
+        videoY: 0,
+        videoRatio: 1280/1080,
+        mediasPath: 'https://files.daviddarx.com/works/static/home/'
       }
     },
     mounted () {
       setTimeout(this.display, 100);
+
+      window.addEventListener('resize', this.resizeListener);
+      this.resizeListener();
     },
 
     methods: {
-      display: function() {
+      display: function () {
         if (this.$parent.isLoaded == true) {
           this.isDisplayed = true;
         }
+      },
+      resizeListener: function () {
+        this.stageW = this.$refs.stage.$el.offsetWidth;
+        this.stageH = this.$refs.stage.$el.offsetHeight;
+        this.stageRatio = this.stageW / this.stageH;
+
+        if (this.stageRatio > this.videoRatio) {
+          this.videoW = this.stageW;
+          this.videoH = this.videoW / this.videoRatio;
+          this.videoX = 0;
+          this.videoY = (this.videoH - this.stageH) * -0.5;
+        } else {
+          this.videoH = this.stageH;
+          this.videoW = this.videoH * this.videoRatio;
+          this.videoY = 0;
+          this.videoX = (this.videoW - this.stageW) * -0.5;
+        }
+
+        console.log(this.videoW + " " + this.videoH);
+        console.log(this.videoX + " " + this.videoY);
+
+        this.$refs.video.style.setProperty('--w', this.videoW + "px");
+        this.$refs.video.style.setProperty('--h', this.videoH + "px");
+        this.$refs.video.style.setProperty('--x', this.videoX + "px");
+        this.$refs.video.style.setProperty('--y', this.videoY + "px");
+
       }
     }
   });
